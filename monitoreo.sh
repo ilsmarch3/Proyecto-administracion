93461d041567d7696081e66bf48fa79f52191130
Ilse <ilsemartinezchimal@gmail.com>
2026-06-22 16:59:10 -0600

    listo

--- monitoreo.sh
+++ monitoreo.sh
@@ -2,16 +2,20 @@
 
 source config.txt
 
+#Permite modificar los umbrales mediante argumentos
 cpu=${1:-$UMBRAL_CPU}
 disco=${2:-$UMBRAL_DISCO}
 
+#Obtiene el porcentaje actual de uso de CPU y disco
 uso_cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print 100-$8}')
 uso_cpu=${uso_cpu%.*}
 
 uso_disco=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
 
+#Registra el uso de CPU y disco en el log
 echo "$(date) CPU:$uso_cpu Disco:$uso_disco" >> "$LOG"
 
+#Verifica si el uso de CPU o disco supera los umbrales y envía una alerta a Telegram
 if [ "$uso_cpu" -gt "$cpu" ]; then
 
     curl -s -X POST \
