b3b5b8b53aa1e718d637420c0c5f1f7667fcace3
Ilse <ilsemartinezchimal@gmail.com>
2026-06-22 17:09:04 -0600

    listo

--- remoto.sh
+++ remoto.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+hosts=$1
+script=$2
+
+#Verifica que se proporcionen los argumentos necesarios
 if [ $# -ne 2 ]; then
     echo "Uso: $0 hosts.txt script.sh"
     exit 1
 fi
-
-hosts=$1
-script=$2
 
 while read host
 do
 
     echo "Conectando a $host"
 
+    #Copia el script al equipo remoto mediante SCP
     scp "$script" "$host:/tmp/" &>/dev/null
-
+    
     salida=$(ssh "$host" "bash /tmp/$(basename $script)")
 
     echo "$salida" > "reporte_$host.txt"
